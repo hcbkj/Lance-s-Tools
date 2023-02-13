ae4dd0eb432851649976948d68d62c702ba3d53b
hcbkj <442480745@qq.com>
2023-02-13 14:03:52 +0800

    新增tools 关闭selenium提示.py

---
+++ scripts/tools/关闭selenium提示.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/13 14:03
+
+
+_option.add_experimental_option('useAutomationExtension', False)  # 关闭“chrome正受到自动测试软件的控制”
+                _option.add_experimental_option("excludeSwitches", ['enable-automation'])
+                _option.add_argument("--disable-blink-features=AutomationControlled")
